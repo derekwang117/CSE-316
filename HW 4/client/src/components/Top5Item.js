32e3d59418fddad1ffbbc07e299a4aafd4dd1ee3
Derek Wang <toderekwang@gmail.com>
2021-11-07 20:55:51 -0500

    Part 8: Item Editting- Complete
    Added item editing and undo/redo functionality to it

--- HW 4/client/src/components/Top5Item.js
+++ HW 4/client/src/components/Top5Item.js
@@ -14,6 +14,7 @@ import EditIcon from '@mui/icons-material/Edit';
 function Top5Item(props) {
     const { store } = useContext(GlobalStoreContext);
     const [editActive, setEditActive] = useState(false);
+    const [text, setText] = useState("");
     const [draggedTo, setDraggedTo] = useState(0);
 
     function handleDragStart(event, targetId) {
@@ -45,7 +46,9 @@ function Top5Item(props) {
         console.log("handleDrop (sourceId, targetId): ( " + sourceId + ", " + targetId + ")");
 
         // UPDATE THE LIST
-        store.addMoveItemTransaction(sourceId, targetId);
+        if (sourceId != targetId) {
+            store.addMoveItemTransaction(sourceId, targetId);
+        }
     }
 
     let { index } = props;
@@ -55,41 +58,87 @@ function Top5Item(props) {
         itemClass = "top5-item-dragged-to";
     }
 
-    return (
-            <ListItem
-                id={'item-' + (index+1)}
-                key={props.key}
-                className={itemClass}
-                onDragStart={(event) => {
-                    handleDragStart(event, (index+1))
-                }}
-                onDragOver={(event) => {
-                    handleDragOver(event, (index+1))
-                }}
-                onDragEnter={(event) => {
-                    handleDragEnter(event, (index+1))
-                }}
-                onDragLeave={(event) => {
-                    handleDragLeave(event, (index+1))
-                }}
-                onDrop={(event) => {
-                    handleDrop(event, (index+1))
-                }}
-                draggable="true"
-                sx={{ display: 'flex', p: 1 }}
-                style={{
-                    fontSize: '48pt',
-                    width: '100%'
-                }}
-            >
+    function handleToggleEdit(event) {
+        event.stopPropagation();
+        toggleEdit();
+    }
+
+    function toggleEdit() {
+        let newActive = !editActive;
+        if (newActive) {
+            store.setIsItemEditActive();
+        }
+        setEditActive(newActive);
+    }
+
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            let id = event.target.id.substring("edit-item-".length);
+            if (text !== "" && props.text !== text) {
+                store.addUpdateItemTransaction(id - 1, text)
+            }
+            toggleEdit();
+        }
+    }
+    function handleUpdateText(event) {
+        setText(event.target.value);
+    }
+
+    let top5itemCard = (
+        <ListItem
+            id={'item-' + (index + 1)}
+            key={props.key}
+            className={itemClass}
+            onDragStart={(event) => {
+                handleDragStart(event, (index + 1))
+            }}
+            onDragOver={(event) => {
+                handleDragOver(event, (index + 1))
+            }}
+            onDragEnter={(event) => {
+                handleDragEnter(event, (index + 1))
+            }}
+            onDragLeave={(event) => {
+                handleDragLeave(event, (index + 1))
+            }}
+            onDrop={(event) => {
+                handleDrop(event, (index + 1))
+            }}
+            draggable="true"
+            sx={{ display: 'flex', p: 1 }}
+            style={{
+                fontSize: '48pt',
+                width: '100%'
+            }}
+        >
             <Box sx={{ p: 1 }}>
-                <IconButton aria-label='edit'>
-                    <EditIcon style={{fontSize:'48pt'}}  />
+                <IconButton onClick={handleToggleEdit} aria-label='edit'>
+                    <EditIcon style={{ fontSize: '48pt' }} />
                 </IconButton>
             </Box>
-                <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
-            </ListItem>
+            <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
+        </ListItem>
     )
+    if (editActive) {
+        top5itemCard =
+            <TextField
+                required
+                fullWidth
+                id={"edit-item-" + (index + 1)}
+                label="Item Name"
+                className='top5-item'
+                onKeyPress={handleKeyPress}
+                onChange={handleUpdateText}
+                defaultValue={props.text}
+                inputProps={{ style: { fontSize: 48 } }}
+                InputLabelProps={{ style: { fontSize: 24 } }}
+                autoFocus
+            />
+    }
+
+    return (
+        top5itemCard
+    );
 }
 
 export default Top5Item;
