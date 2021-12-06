95a1b6398d4e77c27abaa6626f880960fa94b172
Derek Wang <toderekwang@gmail.com>
2021-12-06 18:58:29 -0500

    small bug with app banner auth.user.userName
    cant get userName if user doesnt exist

--- HW 5/client/src/components/AppBanner.js
+++ HW 5/client/src/components/AppBanner.js
@@ -92,7 +92,7 @@ export default function AppBanner() {
     if (auth.user) {
         menuIcon = auth.user.firstName.charAt(0) + auth.user.lastName.charAt(0)
     }
-    if (auth.user.userName === "GuestGuestGuestGuestGuestGuestGuestGuestGuestGuest") {
+    if (auth.user && auth.user.userName === "GuestGuestGuestGuestGuestGuestGuestGuestGuestGuest") {
         menuIcon = "Guest"
     }
 
